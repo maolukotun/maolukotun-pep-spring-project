a2403d6bceb5bf2ffffaba912ed70d42e4fac1bb
Mubarak Olukotun <olukotunmubarak@gmail.com>
2025-06-20 13:58:52 +0000

    Comit Lab Spring Project

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,26 @@
 package com.example.service;
 
-public class MessageService {
+import java.util.List;
+
+import com.example.entity.Message;
+
+
+public interface MessageService {
+
+    List<Message> getAllMessages();
+
+    Message getMessageById(Integer id);
+
+    // Message saveMessage(Message message);
+
+    Message updateMessage(Integer id, Message message);
+
+    void deleteMessage(Integer id);
+
+    Message createMessage(Message message);
+
+    int deleteMessageById(Integer messageId);
+
+    List<Message> getAllMessagesForUser(Integer userId);
+
 }
